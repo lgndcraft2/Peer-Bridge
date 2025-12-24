22c2e3aa171ac3ee8caa9d1d8fb24865408b5bd6
Raheem Akapo <akapsyjr@gmail.com>
2025-12-24 06:44:02 +0100

    God Please

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // @ts-expect-error - turbopack is a valid config but might be missing from types
   turbopack: {},
 };
 
